b43eaf0493e3e117f73e57d1a0ab4124351ba131
redcorjo <jordipromotions@gmail.com>
2023-01-06 01:19:11 +0100

    changes 0.0.2

--- scripts/upload_pypi.sh
+++ scripts/upload_pypi.sh
@@ -3,6 +3,7 @@
 BASE_DIR=$(dirname $0)/..
 TWINE_CONFIG=$(dirname $0)/../.pypirc
 PYTHON=python3.7
+PACKAGE=pyarchetype
 
 cd ${BASE_DIR}
 
@@ -25,12 +26,14 @@ echo "Build"
 python -m build
 
 echo "Upload to pypi"
-if test -e $TWINE_CONFIG
+if test -e ${TWINE_CONFIG}
 then
     chmod 600 ${TWINE_CONFIG}
-    twine upload dist/* --config-file $TWINE_CONFIG
+    twine upload dist/${PACKAGE}* --config-file ${TWINE_CONFIG} 
 else
     twine upload dist/*
 fi
+
+python -m pip install --upgrade dist/${PACKAGE}*whl
 
 exit
