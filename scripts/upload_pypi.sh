5efe882577d8ba94cf3b65de962f3bc04f0bb65c
redcorjo <jordipromotions@gmail.com>
2023-01-06 01:09:58 +0100

    changes

--- scripts/upload_pypi.sh
+++ scripts/upload_pypi.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-BASE_DIR=$(dirname -- "$0")/..
-TWINE_CONFIG=$(dirname -- "$0")/../.pypirc
+BASE_DIR=$(dirname $0)/..
+TWINE_CONFIG=$(dirname $0)/../.pypirc
+PYTHON=python3.7
 
-source ${BASE_DIR}/.venv/bin/activate
+cd ${BASE_DIR}
 
-twine upload dist/* --config-file $TWINE_CONFIG 
+echo "Cleanup old dist files"
+test -e ${BASE_DIR}/dist/*.whl && rm ${BASE_DIR}/dist/* 
+
+if ! test -e ${BASE_DIR}/.venv/bin/activate
+then
+    echo "Creating default virtual environment"
+    ${PYTHON} -m venv .venv
+    echo "Enable virtual environment"
+    source ${BASE_DIR}/.venv/bin/activate
+    python -m pip install --upgrade pip build twine
+else
+    echo "Enable virtual environment"
+    source ${BASE_DIR}/.venv/bin/activate
+fi
+
+echo "Build"
+python -m build
+
+echo "Upload to pypi"
+if test -e $TWINE_CONFIG
+then
+    chmod 600 ${TWINE_CONFIG}
+    twine upload dist/* --config-file $TWINE_CONFIG
+else
+    twine upload dist/*
+fi
 
 exit
